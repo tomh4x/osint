cc4d746b45693c852b90b8d49f01231f1ede2089
tomh4x <tkeigher@foregroundsecurity.com>
2014-02-22 19:53:35 -0500

    fixed STDIN mode for cidr.py

--- cidr.py
+++ cidr.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python
 from netaddr import *
 import sys
-with open(sys.argv[1]) as fh:
-	for content in fh:
-		for ip in IPNetwork(content):
-			print('%s' % ip)
+
+if len(sys.argv) != 2:
+	print(sys.argv[0] + " <filename> or - to read from stdin")
+	sys.exit(1)
+
+if sys.argv[1] == "-":
+	fh = sys.stdin
+else:
+	fh = open(sys.argv[1],"r")
+for content in fh:
+	for ip in IPNetwork(content):
+		print('%s' % ip)
 
